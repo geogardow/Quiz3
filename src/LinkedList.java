13e6af8a1a623cec264697b04be2f08ec2f707d3
geogardow <85602062+geogardow@users.noreply.github.com>
2021-09-09 01:12:14 -0600

    Arreglo problemas de importación

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -32,23 +32,4 @@ public class LinkedList {
             return null;
         }
     }
-
-    /*
-    public static void main(String[] args) {
-        // Create an empty linked list
-        LinkedList linked_list = new LinkedList();
-
-        // Insert some elements in the list
-        linked_list.insertFirst(3);
-        linked_list.insertFirst(8);
-        linked_list.insertFirst(9);
-        linked_list.insertFirst(1);
-
-        // Get and print the size of the list.
-        // It should match the number of elements we inserted
-        int size = linked_list.size();
-
-        System.out.println(size);
-    }
-    */
 }
