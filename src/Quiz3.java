b164c59d387ee3c8ffeea8d146d4cfe63db381f9
geogardow <85602062+geogardow@users.noreply.github.com>
2021-09-09 02:45:44 -0600

    Comentarios de línea

--- src/Quiz3.java
+++ src/Quiz3.java
@@ -1,9 +1,10 @@
 public class Quiz3 {
     private Queue queue;
 
-
     public static void main(String[] args) {
+        // Se crea el Queue
         Queue queue = new Queue();
+        //Se agregan elementos al queue
         queue.enqueue(5);
         queue.enqueue(8);
         queue.enqueue(3);
@@ -11,19 +12,20 @@ public class Quiz3 {
         queue.enqueue(0);
         queue.enqueue(89);
         queue.enqueue(77);
-        
+        //Se imprime el queque
         System.out.println("El Queue se imprime a continuación");
         printQueue(queue);
-
+        //Se hace dequeue
         System.out.println("\nElementos eliminados");
         System.out.println(queue.dequeue().getData());
         System.out.println(queue.dequeue().getData());
         System.out.println(queue.dequeue().getData());
-
+        //Se imprime nuevamente
         System.out.println("\nEl Queue se imprime nuevamente");
         printQueue(queue);
     }
     public static void printQueue(Queue queue){
+        //Función peek, toma los elementos sin eliminarlos
         Node a;
         a = queue.peek();
         while (a!=null){
